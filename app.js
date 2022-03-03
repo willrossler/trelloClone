30d0498b652fa26dbd19bd9a5c561cd22c4db934
William Rössler <71815793+willrossler@users.noreply.github.com>
2022-03-03 13:41:21 +0100

    fixed widget, again

--- app.js
+++ app.js
@@ -1,132 +1,106 @@
 //SELECTORS
-window.onload=function() {
+window.onload = function () {
+  const todoInput = document.querySelector("#todo");
+  const todoDescription = document.querySelector("#description");
+  const deadline = document.querySelector("#deadline");
+  const saveButton = document.querySelector("#saveButton");
+  const widgetList = document.querySelector("#widgetList");
+  const tabs1 = document.querySelector("#tabs-1");
+  const tabs2 = document.querySelector("#tabs-2");
+  const tabs3 = document.querySelector("#tabs-3");
+  const formID = document.querySelector("#newTodoForm");
+  const inputs = document.getElementsByTagName("input");
 
-const todoInput = document.querySelector('#todo');
-const todoDescription = document.querySelector('#description');
-const deadline = document.querySelector('#deadline');
-const saveButton = document.querySelector('#saveButton');
-const widgetButton = document.querySelector('#widgetButton');
-const widgetList = document.querySelector('#widgetList');
-const tabs1 = document.querySelector('#tabs-1');
-const tabs2 = document.querySelector('#tabs-2');
-const tabs3 = document.querySelector('#tabs-3');
-const formID = document.querySelector('#newTodoForm');
-const inputs = document.getElementsByTagName('input');
+  //FUNCTION FOR OPENING MODAL/TAB & SETTING VALUES TO TABS
 
+  function openModal(event) {
+    $("#tabs").dialog();
+    tabs1.innerText = event.target.getAttribute("data-todo");
+    tabs2.innerText = event.target.getAttribute("data-description");
+    tabs3.innerText = event.target.getAttribute("data-deadline");
+  }
 
-
-//FUNCTION FOR OPENING MODAL/TAB & SETTING VALUES TO TABS
-
-  function openModal(event){
-      $('#tabs').dialog();   
-      tabs1.innerText = event.target.getAttribute("data-todo");
-      tabs2.innerText = event.target.getAttribute("data-description");
-      tabs3.innerText = event.target.getAttribute("data-deadline");
-  };
-
-
-
-    //EVENTS
-saveButton.addEventListener('click', addTodo);
-widgetButton.addEventListener('click', widgetFetch);
-
-
-//FUNCTION FOR TODO LIST
-function addTodo(event){
+  //EVENTS
+  saveButton.addEventListener("click", addTodo);
+  //FUNCTION FOR TODO LIST
+  function addTodo(event) {
     event.preventDefault();
 
     for (var i = 0; i < inputs.length; i++) {
-      if(inputs[i].value == ""){
+      if (inputs[i].value == "") {
         alert("Please fill all fields");
         return false;
       }
-      }
-    
+    }
+
     // Todo DIV
-    const todoDiv = document.querySelector('#sortable1')
+    const todoDiv = document.querySelector("#sortable1");
 
     // CREATE LI
-    const newTodo = document.createElement('li');
+    const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todoItem');
+    newTodo.classList.add("todoItem");
     newTodo.setAttribute("data-description", todoDescription.value);
     newTodo.setAttribute("data-todo", todoInput.value);
     newTodo.setAttribute("data-deadline", deadline.value);
-    newTodo.addEventListener('click', openModal);
+    newTodo.addEventListener("click", openModal);
     todoDiv.appendChild(newTodo);
-    
 
-    $('#modal').dialog('close');
+    $("#modal").dialog("close");
     formID.reset();
+  }
 
-    
-};
+  //JQUERY
 
-function widgetFetch(event) {
-  $.ajax({
-    url: "https://www.boredapi.com/api/activity",
-    contenType: 'application/json',
-    error: function(err) {
-      console.log('something went wrong', err);
-    },
-    success: function(result){
-      const widgetText = document.createElement('li');
-      widgetText.innerText = result.activity;
-      widgetList.append(widgetText);
-    }
-});
-}
-
-//JQUERY
-$(function() {
-    $(document).ready( function(){
-      $('#modal').dialog({autoOpen: false, hide: true, show: true});
+  $(function () {
+    $(document).ready(function () {
+      $("#modal").dialog({ autoOpen: false, hide: true, show: true });
     });
-    
-    $( function() {
-        $( "#tabs" ).tabs();
-      });
 
-    $('#newButton').click(function () {
-      $('#modal').dialog('open');
-
-    });
-       
-    $(function() {
-        $("#sortable1, #sortable2, #sortable3" ).sortable({
-          connectWith: ".sortableCol"
-        })
-      });
-    
-      $(function() {
-        $(".datepicker").datepicker({ 
-            dateFormat: "yy-mm-dd", 
-            onSelect: function(){
-                var selectedDate = $(this).val();
-                console.log(selectedDate);
-            }
+    $.widget("custom.fetchText", {
+      _create: function () {
+        $.ajax({
+          url: "https://www.boredapi.com/api/activity",
+          contenType: "application/json",
+          error: function (err) {
+            console.log("something went wrong", err);
+          },
+          success: function (result) {
+            const widgetText = document.createElement("li");
+            widgetText.innerText = result.activity;
+            widgetList.append(widgetText);
+          },
         });
-      
+      },
+    });
 
-      $( function() {
-        $( '#title' ).colorMyStuff();
+    $("#widgetButton").on("click", function () {
+      $("#widgetButton").fetchText();
+    });
+
+    $(function () {
+      $("#tabs").tabs();
+    });
+
+    $("#newButton").click(function () {
+      console.log("first");
+      $("#modal").dialog("open");
+    });
+
+    $(function () {
+      $("#sortable1, #sortable2, #sortable3").sortable({
+        connectWith: ".sortableCol",
       });
     });
 
-// Mandatory UI Widget
-
-  
-
-
-$.fn.colorMyStuff = function() {
-  let settings = $.extend({
-    fontSize : '50px',
-  }, );
-
-  return this.css({
-    'font-size': settings.fontSize,
-    'text-decoration': settings.textdecoration
+    $(function () {
+      $(".datepicker").datepicker({
+        dateFormat: "yy-mm-dd",
+        onSelect: function () {
+          var selectedDate = $(this).val();
+          console.log(selectedDate);
+        },
+      });
+    });
   });
-}
-});
 };
